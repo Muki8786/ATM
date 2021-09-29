c4d76b6fe542b3aa7c6c5198a805558ebb001984
Muki8786 <balajimukesh8786@gmail.com>
2021-09-29 11:18:12 +0530

    Edit

--- src/Account.java
+++ src/Account.java
@@ -11,7 +11,7 @@ public class Account {
     private float balance;
     private int admin;
     private List<String> transactionHistories;
-    private boolean val;
+    private boolean isTrueOrFalse ;
 
 
 
